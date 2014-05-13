88869797c27efdcb5ed7b715f0376f251c69ddd0
joshWyatt <joshuawyatt123@gmail.com>
2014-05-12 21:44:42 -0700

    Implemented 'searchForClassNameRecursively' inside 'getElementsByClass' to recursively search childNodes of document.body for childNodes (with childNode, etc.) and store store and return any node that has the classname passed to 'getElementsByClass'. Function currently passes all test parameters.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,13 +1,4 @@
 // If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
-
-// document.body.childNodes[].nodeName.toLowerCase();
-// document.body.childNodes[].classList;
-
-// if (childNodes.length)
-
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
 
@@ -36,10 +27,23 @@ var getElementsByClassName = function(className){
 		var hasChildNodes = function(node) {
 			return node.childNodes.length > 0;
 		};
+
+		var results = [];
+
+		var searchForClassNameRecursively = function(node, className) {
+			if (hasTargetClass(className, node)) {
+				results.push(node);
+			}
+			if (hasChildNodes(node)) {
+				forEach(node.childNodes, function(childNode) {
+					if (typeof childNode === 'object') {
+						searchForClassNameRecursively(childNode, className);
+					}
+				});
+			}
+		}
+
+		searchForClassNameRecursively(document.body, className);
+		return results;
 };
-
-var a = dbc[2].childNodes[0].childNodes[0].childNodes[0].childNodes.length;
-var b = document.getElementsByClassName('targetClassName');
-
-console.log(b);
 
